121ecac4551048c0ef8bea57a7aeff336e240693
Storm <storm.mathisen@stfc.ac.uk>
2022-08-07 20:47:25 +0100

    Waveform iterator and next day file switch

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+//std imports
 use std::io::prelude::*;
-use std::io::Cursor;
 use std::io::{SeekFrom, BufReader};
+use std::{thread, time};
+use std::fs::File;
+use std::sync::mpsc::{channel, Receiver};
+
 use chrono::prelude::*;
 use chrono::Duration;
-use hdf5::{File as HFile, Group, H5Type, Result};
-use std::{thread, time};
-use std::mem;
-use std::fs::File;
-use std::io::prelude::*;
-use std::sync::mpsc::{channel, Sender, Receiver};
+use byteorder::{LittleEndian, ReadBytesExt};
+use hdf5::{File as HFile};
 
+//TODO: External config file
+//TODO: Error handling at all .unwrap()
 
-const samples: usize = 512; //Number of samples in each array
-const test_file: &str = "/home/storm/Desktop/hdf5rustlocal/example_register"; //Location of test-register
-const adc_offset: u64 = 160; //Offset to first ADC array
-const adc_length: u64 = 128; //Offset between ADC arrays
-const adc_num: u64= 10; //Number of ADCs
-const runtime: i64 = 1; //Runtime in minutes
-const adc_offsets: [u64;  adc_num as usize] = [160, 240, 176, 256, 192, 272, 208, 288, 224, 304]; //Offsets for each ADC
-const active_pulse_offset: u64 = 70; //Offset for active pulse registry
-const total_pulse_offset: u64 = 71; //Offset for total pulse registry
-const state_offset: u64 = 66; //Offset for stat
-const chunk_size: usize = 512; //HDF5 chunk size
+const SAMPLES: usize = 512; //Number of samples in each array
+const TEST_FILE: &str = "/home/storm/Desktop/hdf5rustlocal/example_register"; //Location of test-register
+const ADC_OFFSET: u64 = 160; //Offset to first ADC array
+const ADC_LENGTH: u64 = 128; //Offset between ADC arrays
+const ADC_NUM: u64= 10; //Number of ADCs
+const RUNTIME: i64 = 1; //Runtime in minutes
+const ADC_OFFSETS: [u64;  ADC_NUM as usize] = [160, 240, 176, 256, 192, 272, 208, 288, 224, 304]; //Offsets for each ADC
+const ACTIVE_PULSE_OFFSET: u64 = 70; //Offset for active pulse registry
+const TOTAL_PULSE_OFFSET: u64 = 71; //Offset for total pulse registry
+const STATE_OFFSET: u64 = 66; //Offset for stat
+const CHUNK_SIZE: usize = 512; //HDF5 chunk size
+const DATA_FIELD_NAMES: [&str; ADC_NUM as usize] = [
+    "kly_fwd_pwr",
+    "kly_fwd_pha",
+    "kly_rev_pwr",
+    "kly_rev_pha",
+    "cav_fwd_pwr",
+    "cav_fwd_pha",
+    "cav_rev_pwr",
+    "cav_rev_pha",
+    "cav_probe_pwr",
+    "cav_probe_pha"
+];
 
 
 #[derive(Debug)]
@@ -32,16 +45,37 @@ struct DataContainer {
     active_pulse: u32,
     total_pulse: u32,
     state: u32,
-    kly_fwd_pwr: [u16; samples],
-    kly_fwd_pha: [u16; samples],
-    kly_rev_pwr: [u16; samples],
-    kly_rev_pha: [u16; samples],
-    cav_fwd_pwr: [u16; samples],
-    cav_fwd_pha: [u16; samples],
-    cav_rev_pwr: [u16; samples],
-    cav_rev_pha: [u16; samples],
-    cav_probe_pwr: [u16; samples],
-    cav_probe_pha: [u16; samples]
+    kly_fwd_pwr: [u16; SAMPLES],
+    kly_fwd_pha: [u16; SAMPLES],
+    kly_rev_pwr: [u16; SAMPLES],
+    kly_rev_pha: [u16; SAMPLES],
+    cav_fwd_pwr: [u16; SAMPLES],
+    cav_fwd_pha: [u16; SAMPLES],
+    cav_rev_pwr: [u16; SAMPLES],
+    cav_rev_pha: [u16; SAMPLES],
+    cav_probe_pwr: [u16; SAMPLES],
+    cav_probe_pha: [u16; SAMPLES]
+}
+
+impl IntoIterator for DataContainer {
+    type Item = [u16; SAMPLES];
+    type IntoIter = std::array::IntoIter<Self::Item, 10>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let iter_array = [
+            self.kly_fwd_pwr,
+            self.kly_fwd_pha,
+            self.kly_rev_pwr,
+            self.kly_rev_pha,
+            self.cav_fwd_pwr,
+            self.cav_fwd_pha,
+            self.cav_rev_pwr,
+            self.cav_rev_pha,
+            self.cav_probe_pwr,
+            self.cav_probe_pha
+        ];
+        iter_array.into_iter()
+    }
 }
 
 
@@ -49,8 +83,8 @@ fn main() -> std::io::Result<()> {
 
     //Initialization
     let prog_start = Utc::now();
-    let prog_stop = prog_start+Duration::minutes(runtime);
-    let reg_file = std::fs::File::open(test_file)?;
+    let prog_stop = prog_start+Duration::minutes(RUNTIME);
+    let reg_file = File::open(TEST_FILE)?;
     let mut reg_reader = BufReader::new(reg_file);
     let mut internal_counter: u64 = 0;
 
@@ -69,22 +103,22 @@ fn main() -> std::io::Result<()> {
 
         let shot_timestamp = Utc::now();
 
-        let mut data_container = DataContainer{
+        let data_container = DataContainer{
             internal_count: internal_counter,
             datetime: shot_timestamp,
-            active_pulse: read_register_offset(active_pulse_offset, &mut reg_reader) as u32,
-            total_pulse: read_register_offset(total_pulse_offset, &mut reg_reader) as u32,
-            state: read_register_offset(state_offset, &mut reg_reader) as u32,
-            kly_fwd_pwr: read_array_offset(samples, adc_offsets[0], &mut reg_reader),
-            kly_fwd_pha: read_array_offset(samples, adc_offsets[1], &mut reg_reader),
-            kly_rev_pwr: read_array_offset(samples, adc_offsets[2], &mut reg_reader),
-            kly_rev_pha: read_array_offset(samples, adc_offsets[3], &mut reg_reader),
-            cav_fwd_pwr: read_array_offset(samples, adc_offsets[4], &mut reg_reader),
-            cav_fwd_pha: read_array_offset(samples, adc_offsets[5], &mut reg_reader),
-            cav_rev_pwr: read_array_offset(samples, adc_offsets[6], &mut reg_reader),
-            cav_rev_pha: read_array_offset(samples, adc_offsets[7], &mut reg_reader),
-            cav_probe_pwr: read_array_offset(samples, adc_offsets[8], &mut reg_reader),
-            cav_probe_pha: read_array_offset(samples, adc_offsets[9], &mut reg_reader)
+            active_pulse: read_register_offset(ACTIVE_PULSE_OFFSET, &mut reg_reader) as u32,
+            total_pulse: read_register_offset(TOTAL_PULSE_OFFSET, &mut reg_reader) as u32,
+            state: read_register_offset(STATE_OFFSET, &mut reg_reader) as u32,
+            kly_fwd_pwr: read_array_offset(SAMPLES, ADC_OFFSETS[0], &mut reg_reader),
+            kly_fwd_pha: read_array_offset(SAMPLES, ADC_OFFSETS[1], &mut reg_reader),
+            kly_rev_pwr: read_array_offset(SAMPLES, ADC_OFFSETS[2], &mut reg_reader),
+            kly_rev_pha: read_array_offset(SAMPLES, ADC_OFFSETS[3], &mut reg_reader),
+            cav_fwd_pwr: read_array_offset(SAMPLES, ADC_OFFSETS[4], &mut reg_reader),
+            cav_fwd_pha: read_array_offset(SAMPLES, ADC_OFFSETS[5], &mut reg_reader),
+            cav_rev_pwr: read_array_offset(SAMPLES, ADC_OFFSETS[6], &mut reg_reader),
+            cav_rev_pha: read_array_offset(SAMPLES, ADC_OFFSETS[7], &mut reg_reader),
+            cav_probe_pwr: read_array_offset(SAMPLES, ADC_OFFSETS[8], &mut reg_reader),
+            cav_probe_pha: read_array_offset(SAMPLES, ADC_OFFSETS[9], &mut reg_reader)
         };
         let _data_result = datasender.send(data_container).unwrap();
         internal_counter += 1;
@@ -102,7 +136,7 @@ fn main() -> std::io::Result<()> {
 
     }
     let _send_status = controlsender.send(true).unwrap();
-    handler.join();
+    let _handler_status = handler.join().unwrap();
     Ok(())
 }
 
@@ -125,12 +159,24 @@ fn write_data_hdf5(hdffile: &HFile, data: DataContainer) {
     let attr = hdfgroup.new_attr::<u64>().shape([1]).create("datetime").unwrap();
     attr.write(&[data.datetime.timestamp_nanos()]).unwrap();
 
-    let ds_builder = hdfgroup
+/*     let ds_builder = hdfgroup
         .new_dataset_builder()
-        .chunk((chunk_size));
+        .chunk((CHUNK_SIZE));
     let ds = ds_builder
         .with_data(&data.kly_fwd_pwr)
         .create("kly_fws_pwr");
+ */    
+    //TODO: Test!!!
+    let mut n = 0;
+    for wave in data {
+    let ds_builder = hdfgroup
+        .new_dataset_builder()
+        .chunk((CHUNK_SIZE));
+    let _ds = ds_builder
+        .with_data(&wave)
+        .create(DATA_FIELD_NAMES[n]);
+        n += 1;
+    }
 
     let write_dur: [u64; 1] = [write_start.elapsed().as_micros() as u64];
 
@@ -141,10 +187,19 @@ fn write_data_hdf5(hdffile: &HFile, data: DataContainer) {
 fn write_hdf5_thread(controlreceiver: Receiver<bool>, datareceiver: Receiver<DataContainer>) {
     let mut thread_counter = 0;
     let start: DateTime<Utc> = Utc::now();
-    let hdffile = HFile::create(&start.format("/home/storm/Desktop/hdf5rustlocal/%Y-%m-%d %H:%M:%S.h5").to_string()).unwrap();
+    let mut next_day = start.date().succ().and_hms(0,0,0);
+    let mut hdffile = HFile::create(&start.format("/home/storm/Desktop/hdf5rustlocal/%Y-%m-%d %H:%M:%S.h5").to_string()).unwrap();
 
     loop {
         //println!("Thread is here!");
+        let now = Utc::now();
+        //Check if past midnight, create new file if yes
+        //TODO: Implement move old file to network storage!
+        if now > next_day {
+            next_day = now.date().succ().and_hms(0,0,0);
+            hdffile.close();
+            hdffile = HFile::create(&now.format("/home/storm/Desktop/hdf5rustlocal/%Y-%m-%d %H:%M:%S.h5").to_string()).unwrap();
+        }
         thread_counter += 1;
         let received_data = datareceiver.recv_timeout(time::Duration::from_millis(25)).unwrap();
         write_data_hdf5(&hdffile, received_data);
@@ -172,8 +227,8 @@ fn write_hdf5_thread(controlreceiver: Receiver<bool>, datareceiver: Receiver<Dat
     }
 }
 
-fn read_array_offset(read_len: usize, offset: u64, buffer: &mut BufReader<std::fs::File>) -> [u16; samples] {
-    let mut out_buffer: [u16; samples] = [0; samples];
+fn read_array_offset(read_len: usize, offset: u64, buffer: &mut BufReader<std::fs::File>) -> [u16; SAMPLES] {
+    let mut out_buffer: [u16; SAMPLES] = [0; SAMPLES];
     let _ret = buffer.seek(SeekFrom::Start(offset));
     let _ret = buffer.read_u16_into::<LittleEndian>(&mut out_buffer).unwrap();
     out_buffer
